7d87977ce3532a817e18ca0abc603a9d94580a9c
熊仔 <52132077+xiong-ling@users.noreply.github.com>
2022-05-20 14:00:48 +0800

    Create forEach.js

---
+++ array/forEach.js
@@ -0,0 +1,44 @@
+/**
+ * forEach() 方法对数组的每个元素执行一次给定的函数。
+ */
+
+/**
+ * @param {Array} array
+ * @param {Function} callback(currentValue, index, array)
+ * @param {Object} thisArg
+ */
+
+function forEach(array, callback, thisArg) {
+    if (!Array.isArray(array)) {
+        throw new Error('function forEach expect Array, but get other');
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        const value = array[i];
+        callback.call(thisArg, value, i, thisArg)
+    }
+}
+
+/** test测试用例 */
+forEach([1, 2], function (value, index) {
+    console.log(value, index);
+});
+
+
+function Counter() {
+    this.sum = 0;
+    this.count = 0;
+}
+Counter.prototype.add = function (array) {
+    forEach(array, function (entry) {
+        this.sum += entry;
+        ++this.count;
+    }, this);
+};
+
+const obj = new Counter();
+obj.add([2, 5, 9]);
+console.log(obj.count);
+// 3 === (1 + 1 + 1)
+console.log(obj.sum);;
+  // 16 === (2 + 5 + 9)
